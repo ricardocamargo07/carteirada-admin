289dd2bf96084feafd9c68c135bea473a7d066bd
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-08-13 11:43:18 -0300

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,11 @@ Auth::routes();
 
 Route::get('/', ['as' => 'home', 'uses' => 'Home@index']);
 
-Route::get('/clipping', ['as' => 'home', 'uses' => 'Clipping@index']);
+Route::get('/clipping', ['as' => 'clipping.index', 'uses' => 'Clipping@index']);
 
-Route::get('/awards', ['as' => 'home', 'uses' => 'Awards@index']);
+Route::get('/awards', ['as' => 'awards.index', 'uses' => 'Awards@index']);
 
 Route::group(['prefix' => '/partials'], function () {
-    Route::get('/home', ['as' => 'partials.home', 'uses' => 'Home@home']);
-
     Route::get('/lei', ['as' => 'partials.leis', 'uses' => 'Home@leis']);
 
     Route::get('/busca', ['as' => 'partials.busca', 'uses' => 'Home@busca']);
