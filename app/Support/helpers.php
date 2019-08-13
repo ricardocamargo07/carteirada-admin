e023761e152fa126e2db1bf48beaf8ad7fa9e27b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-08-13 12:08:01 -0300

    Fix missing super admin key

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -5,27 +5,14 @@ if (! function_exists('is_super_admin')) {
      * Is the user a super admin?
      *
      * @param null $user
-     * @return void
+     * @return bool
      */
     function is_super_admin($user = null)
     {
         $user = $user ?: \Auth::user()->email;
 
-        return strpos($user, config('app.users.super_admins')) !== false;
-    }
-}
+        $admins = config('app.users.super_admins');
 
-if (! function_exists('to_pt_br_date')) {
-    /**
-     * Is the user a super admin?
-     *
-     * @param null $user
-     * @return void
-     */
-    function to_pt_br_date($user = null)
-    {
-        $user = $user ?: \Auth::user()->email;
-
-        return strpos($user, config('app.users.super_admins')) !== false;
+        return filled($admins) && strpos($user, $admins) !== false;
     }
 }
